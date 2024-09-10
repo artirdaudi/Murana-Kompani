4b1f6ca07cfba980c0616e34cfb30276dbfd5d10
Artir Daudi <ad28783@seeu.edu.mk>
2024-09-10 17:03:27 +0200

    Added Folder for Macedonian Language

---
+++ mk/Sherbimet/js/scripts.js
@@ -0,0 +1,7 @@
+// Toggle the navbar on mobile view
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
+
+
